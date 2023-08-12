a99d41481544662f81f0fa3300bcb5616a597f38
Nbddyy <95341178+Nbddyy@users.noreply.github.com>
2023-08-12 16:30:52 +0800

    Update Edge capture register.v

--- Sequential Logic/Latches and Flip-Flops/Edge capture register.v
+++ Sequential Logic/Latches and Flip-Flops/Edge capture register.v
@@ -13,27 +13,27 @@ module top_module (
     
     genvar i;
     generate for(i = 0; i < 32; i = i + 1) begin : generate_32_fall_detect
-        always@(posedge clk or posedge reset) begin
-            if(reset) begin		//同步清零
-                in_clap_1[i] <= 1'd0;
-            end else begin
-                in_clap_1[i] <= in[i];
-            end
+        always@(posedge clk) begin
+            in_clap_1[i] <= in[i];    //通过对波形图的观察发现，该打拍操作不能受reset影响
         end
         
         assign in_fall[i] = in_clap_1[i] && !in[i];
         
-        /*输出out的代码实现：下降沿拉高保持，直至复位置0*/
-        always@(posedge clk) begin
-            if(reset) begin
-                out[i] = 1'd0;
-            end else if(in_fall[i]) begin
-                out[i] = 1'd1; 
-            end else begin
-                out[i] = out[i]; 
-            end
-        end
-        
     end endgenerate
     
+    
+    genvar j;
+    generate for(j = 0; j < 32; j = j + 1) begin : generate_32_out_result
+    	/*输出out的代码实现：下降沿拉高保持，直至复位置0*/
+        always@(posedge clk) begin
+            if(reset) begin    //此处实现优先级判断，当检测到下降沿时优先复位，后捕获输入信号
+                out[j] = 1'd0;
+            end else if(in_fall[j]) begin
+                out[j] = 1'd1; 
+            end else begin
+                out[j] = out[j]; 
+            end
+        end
+    end endgenerate
+        
 endmodule
